9df0d66fe02adf38d07bc2b40241eb918c24b09b
Nikolay Ivanov <nivanov@tango.me>
2017-03-24 19:24:27 +0300

    update

--- src/main/scala/ResourceQualifier.scala
+++ src/main/scala/ResourceQualifier.scala
@@ -1,6 +1,5 @@
  package com.github.ymkawb.translation_helper.model
- import scala.util.parsing.combinator._
-
+ import com.typesafe.scalalogging._;
  //See https://developer.android.com/guide/topics/resources/providing-resources.html Table 2 for up-to-date list of variation
  case class ResourceQualifier(
  	val mcc_mnc : Option[String] = None,
@@ -8,6 +7,7 @@
  	val layoutDirection : Option[String] = None,
  	val smallestWidth : Option[String] = None,
  	val avalibleWidth : Option[String] = None,
+ 	val avalibleHeight : Option[String] = None,
  	val screenSize : Option[String] = None,
  	val screenAspect : Option[String] = None,
  	val roundScreen : Option[String] = None,
@@ -24,6 +24,43 @@
 
 
  object ResourceQualifier {
- 		def apply(input:String) = ???
+ 	object Builder extends LazyLogging {
+
+ 		import scala.util.matching.Regex
+
+ 		type P = (Regex,((ResourceQualifier,String) => ResourceQualifier))
+ 		type F = (String,ResourceQualifier)
+
+ 		def foldFunc(f:F,p:P) : F = {
+ 			val fn  =  p._2 //Build func 	
+ 			val rq = f._2// 
+ 			p._1.findPrefixOf(f._1) match {
+ 				case Some(x) => {
+ 					val vv = stripHeading(p._1.replaceFirstIn(f._1,""))
+ 					(vv,fn(rq,x))
+ 				}
+ 				case None => f
+ 			} 		
+ 		}
+
+ 		def stripHeading(in:String) : String = in.headOption match {
+ 			case Some('-') => in.drop(1)
+ 			case _ =>  in
+ 		}
+ 		
+ 		val order  = List(
+ 			("""(mcc(\d+))(-mnc\d+)?""".r,	((x:ResourceQualifier,y:String) => x.copy(mcc_mnc = Some(y)))),
+ 			("""(\w\w(-\w\w\w)?)""".r,		((x:ResourceQualifier,y:String) => x.copy(locale = Some(y)))),
+ 			("""(ldrtl)|(ldltr)""".r, 			((x:ResourceQualifier,y:String) => x.copy(layoutDirection = Some(y)))),
+ 			("""sw\d+dp""".r, 				((x:ResourceQualifier,y:String) => x.copy(smallestWidth = Some(y)))),
+ 			("""w\d+dp""".r, 				((x:ResourceQualifier,y:String) => x.copy(avalibleWidth = Some(y)))),
+ 			("""h\d+dp""".r, 				((x:ResourceQualifier,y:String) => x.copy(avalibleHeight = Some(y)))),
+ 			("""(small)|(normal)|(large)|xlarge""".r,((x:ResourceQualifier,y:String) => x.copy(screenSize = Some(y))))
+ 			)
+
+ 		def build(input:String) = order.foldLeft((input,ResourceQualifier()))(foldFunc)._2
 
  	}
+ 	def apply(input:String) : ResourceQualifier = Builder.build(input)
+
+ }
